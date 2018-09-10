d571deb4c4e0fcc8107212d8310cc178bb36097f
Vasil <Vasil_i_Nikolov@abv.bg>
2018-09-10 03:13:49 +0300

    Adding Article methods and fixing Article Controller
    - Adding Setters and Getters to the Article Entity
    - Creating some functions in the Article Controller
    - Creating a unique page for every Artile in the database
    - Fixing .twig files for every page

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,6 +1,8 @@
 <?php
     namespace App\Controller;
 
+    use App\Entity\Article;
+
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
     use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -8,15 +10,45 @@
 
     class ArticleController extends Controller {
         /**
-         * @Route("/")
+         * @Route("/", name="article_list")
          * @Method({"GET"})
          */
         public function index() {
-            //return new Response('<html><body>Hello</body></html>');
-            $articles = ['Article 1', 'Article 2'];
+            
+            $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
+
             return $this->render('articles/index.html.twig', array(
                 'articles' => $articles
             ));
         }
+
+        /**
+         * @Route("/article/{id}", name="article_show")
+         */
+        public function show($id) {
+            $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+            return $this->render('articles/show.html.twig', array('article' => $article));
+        }
+
+        ///**
+        // * @Route("/article/save")
+        // */
+        /*
+        public function save() {
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $article = new Article();
+            $article->setTitle('Article Two');
+            $article->setBody('This is the body for Article Two');
+
+            $entityManager->persist($article);
+
+            $entityManager->flush();
+
+            return new Response('Saves an article with an id = ' . $article->getId() );
+        }
+    */
+
     }
 ?>
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -15,9 +15,35 @@ class Article
      * @ORM\Column(type="integer")
      */
     private $id;
+    
+    /**
+     * @ORM\Column(type="text", length=100)
+     */
+    private $title;
 
-    public function getId()
-    {
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $body;
+
+    // Getters and Setters
+    public function getId() {
         return $this->id;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+
+    public function getBody() {
+        return $this->body;
+    }
+
+    public function setBody($body) {
+        $this->body = $body;
     }
 }
